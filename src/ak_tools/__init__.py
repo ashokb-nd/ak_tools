559228ea13b4be83ad1baf7c1a224aa42fe5a919
B Ashok <batakala.ashok@netradyne.com>
2026-03-27 16:28:44 +0530

    Add download_alerts to __all__ exports in __init__.py

--- src/ak_tools/__init__.py
+++ src/ak_tools/__init__.py
@@ -3,6 +3,7 @@
 #import parallel processor as pp
 
 from ak_tools.parallel_processing import parallel_processor as pp
+from ak_tools.sync_alert import download_alerts
 
-__all__ = ["pp"] #for __all__ import *
+__all__ = ["pp", "download_alerts"] #for __all__ import *
 __version__ = "0.1.0"
